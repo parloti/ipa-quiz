6ec4fee0368a7079780624ac6bff98b348f302db
Alex Parloti <parloti@gmail.com>
2026-01-04 18:38:16 -0300

    Remove home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { QuizHomeComponent } from './quiz-home/quiz-home.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { QuizzesHomeComponent } from './quizzes-home/quizzes-home.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent,
     pathMatch: 'full',
-  },
-  {
-    path: 'quizzes-home',
+    path: '',
     component: QuizzesHomeComponent,
   },
   {
